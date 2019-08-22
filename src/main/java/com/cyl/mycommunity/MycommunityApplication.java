1dfb08443dda7095bbe0a40315b0f0c459aa1e58
cylcool <441073373@qq.com>
2019-08-22 21:55:52 +0800

    add thymeleaf and bootstrap

--- src/main/java/com/cyl/mycommunity/MycommunityApplication.java
+++ src/main/java/com/cyl/mycommunity/MycommunityApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MycommunityApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(MycommunityApplication.class, args);
     }
 
